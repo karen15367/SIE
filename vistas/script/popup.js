cf9d6bfb6c29a6869e3c46b641445ce669a0f80c
Samuel G <l22020779@veracruz.tecnm.mx>
2025-04-16 13:39:07 -0600

    se agrego el popup de avisos y se modifico el index.html para que lo muestre

---
+++ vistas/script/popup.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var openPopupButton = document.getElementById("btnAvisos");
+    var popup = document.getElementById("dvAvisos");
+    var closePopupButton = document.getElementById("closePopup");
+    
+    if (popup) {
+        popup.style.display = "block"; 
+    }
+    
+    if (openPopupButton) {
+        openPopupButton.addEventListener("click", function () {
+            if (popup) {
+                popup.style.display = "block";
+            }
+        });
+    }
+    
+    if (closePopupButton) {
+        closePopupButton.addEventListener("click", function () {
+            if (popup) {
+                popup.style.display = "none";
+            }
+        });
+    }
+
+    if (popup) {
+        window.addEventListener("click", function (event) {
+            if (event.target === popup) {
+                popup.style.display = "none";
+            }
+        });
+    }
+});
