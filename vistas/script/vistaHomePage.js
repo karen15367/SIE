4c7f8e386baea1fd874f840f700dbf35c5c68566
Samuel G <l22020779@veracruz.tecnm.mx>
2025-04-22 14:56:47 -0600

    se revisaron las vistas

--- vistas/script/vistaHomePage.js
+++ vistas/script/vistaHomePage.js
@@ -1,1 +1,33 @@
-"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+    var openPopupButton = document.getElementById("btnAvisos");
+    var popup = document.getElementById("dvAvisos");
+    var closePopupButton = document.getElementById("closePopup");
+    if (openPopupButton) {
+        openPopupButton.addEventListener("click", function () {
+            if (popup) {
+                popup.style.display = "block";
+            }
+        });
+    }
+    if (openPopupButton) {
+        openPopupButton.addEventListener("click", function () {
+            if (popup) {
+                popup.style.display = "block";
+            }
+        });
+    }
+    if (closePopupButton) {
+        closePopupButton.addEventListener("click", function () {
+            if (popup) {
+                popup.style.display = "none";
+            }
+        });
+    }
+    if (popup) {
+        window.addEventListener("click", function (event) {
+            if (event.target === popup) {
+                popup.style.display = "none";
+            }
+        });
+    }
+});
