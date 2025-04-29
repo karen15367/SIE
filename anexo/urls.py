b4959365295046014a56ef89217933f2ea8e3dfd
Samuel G <l22020779@veracruz.tecnm.mx>
2025-04-28 21:53:44 -0600

    creacion de arychivos urls.py en las carpetas anexo y encuesta

---
+++ anexo/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.a1),
+    path('anexo2/', views.a2),
+    path('anexo3/', views.a3),
+    path('anexo4/', views.a4),
+    path('anexo5/', views.a5),
+    path('anexo6/', views.a6),
+    path('anexo7/', views.a7),
+    path('anexo8/', views.a8),
+    path('anexo9/', views.a9),
+    path('anexo10/', views.a10),
+    path('anexo11/', views.a11),
+    path('anexo12/', views.a12),
+]
