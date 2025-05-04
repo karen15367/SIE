1fc6a2dd8dc5a5d20044942f5da3eb85299b42c6
Samuel G <l22020779@veracruz.tecnm.mx>
2025-05-03 20:34:32 -0600

    insertar usaruarios

--- signin/views.py
+++ signin/views.py
@@ -2,6 +2,8 @@ from decouple import config
 from django.shortcuts import render
 from django.conf import settings
 from core.models import Egresado
+from datetime import datetime
+from django.contrib.auth.hashers import make_password
 import uuid
 import resend
 
@@ -9,24 +11,40 @@ import resend
 
 
 def signin(request):
-    print('tipo de request', request.method)
     return render(request, "vistaSignUp.html")
 
 
 def verify(request):
     if request.method == 'POST':
         # todo hacer que al validar el correo se guarde en la base de datos
+        # * para la fecha de nacimiento y egreso
+        fechaN = request.POST.get('fechaNacimiento')
+        # * para el sexo
+        if request.POST.get('sexo') is 'Masculino':
+            sexo = False
+        else:
+            sexo = True
+        # * para el titulo
+        if request.POST.get('titulado') is 'no':
+            titulo = False
+        else:
+            titulo = True
+
         try:
-            tempUser = Egresado(
+            '''
+            #TODO hacer que al validar correo se guarde en la base de datos
+            Egresado.objects.create(
                 curp=request.POST.get('curp'),
                 nombre=request.POST.get('nombre'),
                 noControl=request.POST.get('control'),
                 correo=request.POST.get('correo'),
-                sexo=request.POST.get('sexo'),
-                fechaNacimiento=request.POST.get('fechaNacimiento'),
+                sexo=sexo,
+                fechaNacimiento=datetime.strptime(fechaN, '%Y-%m-%d').date(),
                 carrera=request.POST.get('carrera'),
-                titulado=request.POST.get('titulado'),
-                contraseña=request.POST.get('pwd1'))
+                titulado=titulo,
+                fechaEgreso=datetime.strptime(fechaN, '%Y-%m-%d').date(),
+                contraseña=make_password( request.POST.get('pwd1')))
+            '''
             # TODO hacer que se mande el correo de verificación
             token = str(uuid.uuid4())
             confirm_url = f"http://127.0.0.1:8000/confirmar/{token}/"
@@ -34,20 +52,19 @@ def verify(request):
 
             # TODO PASARSE A RESEND
             resend.api_key = config("RESEND_API_KEY")
-            print(f'{resend.api_key} resend api key')
             params = {
                 "from": "onboarding@resend.dev",
-                "to": [tempUser.correo],
+                "to": [request.POST.get('correo')],
                 "subject": "Hello world",
                 "html": "pvto resend tambien me dio problemas"
             }
-            resend.Emails.send(params)
+            #!resend.Emails.send(params)
 
         except ZeroDivisionError as e:
-            print(f'Error al obtener los datos del email {e}')
+            print(f'{e}')
         finally:
-            print(tempUser.correo)
-    return render(request, "vistaVerificacion.html", {"mensaje": "Revisa tu correo para confirmar tu cuenta."})
+            print(request.POST.get('correo'))
+    return render(request, "vistaVerificacion.html")
 
 
 def confirm_email(request, token):
