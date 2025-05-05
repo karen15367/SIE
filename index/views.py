d28211e3bfe2516d141fc181a97a07e57020d822
Karen Sosa <l22020987@veracruz.tecnm.mx>
2025-05-05 10:02:03 -0600

    identificación de usuarios en la homepage

--- index/views.py
+++ index/views.py
@@ -3,9 +3,18 @@ from django.shortcuts import redirect, render
 # Create your views here.
 def index(request):
     tipo = request.session.get('usuario_tipo')
-    clave = request.session.get('usuario_id')
+    identificador = request.session.get('usuario_id')
     carrera = request.session.get('usuario_carrera')
-    return render(request, 'index.html')
+    
+    if not tipo or not identificador:
+        # Si no hay sesión activa, redirigir al login
+        return redirect('vistaLogin')
+    context = {
+        'tipo': tipo,
+        'identificador': identificador,
+        'carrera': carrera,
+    }
+    return render(request, 'index.html', context)
 
 def cerrar_sesion(request):
     request.session.flush()  # Elimina toda la sesión
