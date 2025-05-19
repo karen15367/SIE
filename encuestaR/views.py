3473453996e796f904841e7b36be997669a34d6a
Samuel G <l22020779@veracruz.tecnm.mx>
2025-05-19 16:53:27 -0600

    graficas s3 empresa

--- encuestaR/views.py
+++ encuestaR/views.py
@@ -63,7 +63,8 @@ def viewE1(request):
         'titulado': {'si': t1, 'no': t2},
         'paquetes': {'si': p1, 'no': p2},
     })
-    
+
+
 def exportarE1(request):
     data = EncuestaS1.objects.all()
 
@@ -85,7 +86,6 @@ def exportarE1(request):
         ])
 
     return response
-
 
 
 def viewE2(request):
@@ -141,82 +141,139 @@ def viewE3(request):
     a3 = EncuestaS3.objects.filter(actividad=3).count()
     a4 = EncuestaS3.objects.filter(actividad=4).count()
 
-    answersE= EncuestaS3Empresa.objects.all()
+    answersE = EncuestaS3Empresa.objects.all()
 
-#* Estudian
+# * Estudian
     b1 = sum(1 for a in answers if (a.actividad ==
-            3 or a.actividad == 2) and a.tipoEstudio == 1)
+                                    3 or a.actividad == 2) and a.tipoEstudio == 1)
     b2 = sum(1 for a in answers if (a.actividad ==
-            3 or a.actividad == 2) and a.tipoEstudio == 2)
+                                    3 or a.actividad == 2) and a.tipoEstudio == 2)
     b3 = sum(1 for a in answers if (a.actividad ==
-            3 or a.actividad == 2) and a.tipoEstudio == 3)
+                                    3 or a.actividad == 2) and a.tipoEstudio == 3)
     b4 = sum(1 for a in answers if (a.actividad ==
-            3 or a.actividad == 2) and a.tipoEstudio == 4)
+                                    3 or a.actividad == 2) and a.tipoEstudio == 4)
     b5 = sum(1 for a in answers if (a.actividad ==
-            3 or a.actividad == 2) and a.tipoEstudio == 5)
-#* trabajan
+                                    3 or a.actividad == 2) and a.tipoEstudio == 5)
+# * trabajan
     t1 = sum(1 for a in answers if (a.actividad ==
-            1 or a.actividad == 3) and a.tipoEstudio == 1)
+                                    1 or a.actividad == 3) and a.tipoEstudio == 1)
     t2 = sum(1 for a in answers if (a.actividad ==
-            1 or a.actividad == 3) and a.tipoEstudio == 2)
+                                    1 or a.actividad == 3) and a.tipoEstudio == 2)
     t3 = sum(1 for a in answers if (a.actividad ==
-            1 or a.actividad == 3) and a.tipoEstudio == 3)
+                                    1 or a.actividad == 3) and a.tipoEstudio == 3)
     t4 = sum(1 for a in answers if (a.actividad ==
-            1 or a.actividad == 3) and a.tipoEstudio == 4)
-    
-    m1 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.medioEmpleo == 1)
-    m2 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.medioEmpleo == 2)
-    m3 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.medioEmpleo == 3)
-    m4 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.medioEmpleo == 4)
-    m5 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.medioEmpleo == 5)
-    
-    r1 = sum(1 for a in answers if (a.actividad ==1 or a.actividad == 3) and a.requisitosContratacion == 1)
-    r2 = sum(1 for a in answers if (a.actividad ==1 or a.actividad == 3) and a.requisitosContratacion == 2)
-    r3 = sum(1 for a in answers if (a.actividad ==1 or a.actividad == 3) and a.requisitosContratacion == 3)
-    r4 = sum(1 for a in answers if (a.actividad ==1 or a.actividad == 3) and a.requisitosContratacion == 4)
-    r5 = sum(1 for a in answers if (a.actividad ==1 or a.actividad == 3) and a.requisitosContratacion == 5)
-    r6 = sum(1 for a in answers if (a.actividad ==1 or a.actividad == 3) and a.requisitosContratacion == 6)
-    r7 = sum(1 for a in answers if (a.actividad ==1 or a.actividad == 3) and a.requisitosContratacion == 7)
-    
-    i1 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.idiomaUtiliza == 1)
-    i2 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.idiomaUtiliza == 2)
-    i3 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.idiomaUtiliza == 3)
-    i4 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.idiomaUtiliza == 4)
-    i5 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.idiomaUtiliza == 5)
-    
-    e1 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.antiguedad == 1)
-    e2 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.antiguedad == 2)
-    e3 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.antiguedad == 3)
-    e4 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.antiguedad == 4)
-    e5 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.antiguedad == 5)
-    
-    s1 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.ingreso == 1)
-    s2 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.ingreso == 2)
-    s3 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.ingreso == 3)
-    s4 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.ingreso == 4)   
+                                    1 or a.actividad == 3) and a.tipoEstudio == 4)
 
-    n1 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 1)
-    n2 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 2)
-    n3 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 3)
-    n4 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 4)
-    n5 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 5)
-    n6 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 6)
+    m1 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.medioEmpleo == 1)
+    m2 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.medioEmpleo == 2)
+    m3 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.medioEmpleo == 3)
+    m4 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.medioEmpleo == 4)
+    m5 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.medioEmpleo == 5)
 
-    c1 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.condicionTrabajo == 1)
-    c2 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.condicionTrabajo == 2)
-    c3 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.condicionTrabajo == 3)
-    c4 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.condicionTrabajo == 4)  
+    r1 = sum(1 for a in answers if (a.actividad == 1 or a.actividad == 3)
+             and a.requisitosContratacion == 1)
+    r2 = sum(1 for a in answers if (a.actividad == 1 or a.actividad == 3)
+             and a.requisitosContratacion == 2)
+    r3 = sum(1 for a in answers if (a.actividad == 1 or a.actividad == 3)
+             and a.requisitosContratacion == 3)
+    r4 = sum(1 for a in answers if (a.actividad == 1 or a.actividad == 3)
+             and a.requisitosContratacion == 4)
+    r5 = sum(1 for a in answers if (a.actividad == 1 or a.actividad == 3)
+             and a.requisitosContratacion == 5)
+    r6 = sum(1 for a in answers if (a.actividad == 1 or a.actividad == 3)
+             and a.requisitosContratacion == 6)
+    r7 = sum(1 for a in answers if (a.actividad == 1 or a.actividad == 3)
+             and a.requisitosContratacion == 7)
 
-    r1 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 1)
-    r2 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 2)
-    r3 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 3)
-    r4 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 4)
-    r5 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 5)
-    r6 = sum(1 for a in answers if ( a.actividad==1 or a.actividad == 3) and a.nivelJerarquico == 6)
-    
+    i1 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.idiomaUtiliza == 1)
+    i2 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.idiomaUtiliza == 2)
+    i3 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.idiomaUtiliza == 3)
+    i4 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.idiomaUtiliza == 4)
+    i5 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.idiomaUtiliza == 5)
+
+    e1 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.antiguedad == 1)
+    e2 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.antiguedad == 2)
+    e3 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.antiguedad == 3)
+    e4 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.antiguedad == 4)
+    e5 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.antiguedad == 5)
+
+    s1 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.ingreso == 1)
+    s2 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.ingreso == 2)
+    s3 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.ingreso == 3)
+    s4 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.ingreso == 4)
+
+    n1 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.nivelJerarquico == 1)
+    n2 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.nivelJerarquico == 2)
+    n3 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.nivelJerarquico == 3)
+    n4 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.nivelJerarquico == 4)
+    n5 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.nivelJerarquico == 5)
+    n6 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.nivelJerarquico == 6)
+
+    c1 = sum(1 for a in answers if (a.actividad ==
+             1 or a.actividad == 3) and a.condicionTrabajo == 1)
+    c2 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.condicionTrabajo == 2)
+    c3 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.condicionTrabajo == 3)
+    c4 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.condicionTrabajo == 4)
+
+    r1 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.nivelJerarquico == 1)
+    r2 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.nivelJerarquico == 2)
+    r3 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.nivelJerarquico == 3)
+    r4 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.nivelJerarquico == 4)
+    r5 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.nivelJerarquico == 5)
+    r6 = sum(1 for a in answers if (a.actividad ==
+            1 or a.actividad == 3) and a.nivelJerarquico == 6)
+
     u1 = EncuestaS3Empresa.objects.filter(tipoOrganismo=1).count()
     u2 = EncuestaS3Empresa.objects.filter(tipoOrganismo=1).count()
     u3 = EncuestaS3Empresa.objects.filter(tipoOrganismo=1).count()
+
+    q1 = EncuestaS3Empresa.objects.filter(sectorPrimario=1).count()
+    q2 = EncuestaS3Empresa.objects.filter(sectorPrimario=2).count()
+    q3 = EncuestaS3Empresa.objects.filter(sectorPrimario=3).count()
+    q4 = EncuestaS3Empresa.objects.filter(sectorPrimario=4).count()
+
+    h1 = EncuestaS3Empresa.objects.filter(sectorSecundario=1).count()
+    h2 = EncuestaS3Empresa.objects.filter(sectorSecundario=2).count()
+    h3 = EncuestaS3Empresa.objects.filter(sectorSecundario=3).count()
+    h4 = EncuestaS3Empresa.objects.filter(sectorSecundario=4).count()
+   
+    y1 = EncuestaS3Empresa.objects.filter(sectorTerciario=1).count()
+    y2 = EncuestaS3Empresa.objects.filter(sectorTerciario=2).count()
+    y3 = EncuestaS3Empresa.objects.filter(sectorTerciario=3).count()
+    y4 = EncuestaS3Empresa.objects.filter(sectorTerciario=4).count()
 
     return render(request, 'layouts/E3.html', {
         'anexo': False,
@@ -225,7 +282,7 @@ def viewE3(request):
         'estudio': {'uno': b1, 'dos': b2, 'tres': b3, 'cuatro': b4, 'cinco': b5},
         'trabajo': {'uno': t1, 'dos': t2, 'tres': t3, 'cuatro': t4},
         'medio': {'uno': m1, 'dos': m2, 'tres': m3, 'cuatro': m4, 'cinco': m5},
-        'requisitos': {'uno': r1, 'dos': r2, 'tres': r3, 'cuatro': r4, 'cinco': r5, 'seis': r6 ,'siete':r7},
+        'requisitos': {'uno': r1, 'dos': r2, 'tres': r3, 'cuatro': r4, 'cinco': r5, 'seis': r6, 'siete': r7},
         'idioma': {'uno': i1, 'dos': i2, 'tres': i3, 'cuatro': i4, 'cinco': i5},
         'empleo': {'uno': e1, 'dos': e2, 'tres': e3, 'cuatro': e4, 'cinco': e5},
         'salario': {'uno': s1, 'dos': s2, 'tres': s3, 'cuatro': s4, },
@@ -233,8 +290,12 @@ def viewE3(request):
         'condicion': {'uno': c1, 'dos': c2, 'tres': c3, 'cuatro': c4, },
         'relacion': {'uno': r1, 'dos': r2, 'tres': r3, 'cuatro': r4, 'cinco': r5, 'seis': r6},
         'empresa': {'uno': u1, 'dos': u2, 'tres': u3, },
+        'sectorp': {'uno': q1, 'dos': q2, 'tres': q3, 'cuatro': q4, },
+        'sectors': {'uno': h1, 'dos': h2, 'tres': h3, 'cuatro': h4, },
+        'sectort': {'uno': y1, 'dos': y2, 'tres': y3, 'cuatro': y4, },
 
     })
+
 
 def exportarE2(request):
     data = EncuestaS2.objects.select_related('folioEncuesta__curp').all()
@@ -261,4 +322,3 @@ def exportarE2(request):
         ])
 
     return response
-
