c2ca5dfd0ec7fcb6d1a09ef253d18a00eb3426a5
Karen Sosa <l22020987@veracruz.tecnm.mx>
2025-05-21 21:22:48 -0600

    arreglos en signinAdmin

--- signinAdmin/urls.py
+++ signinAdmin/urls.py
@@ -1,3 +1,4 @@
+#signinAdmin/urls.py
 from django.urls import path
 from . import views
 from django.urls import include
@@ -5,6 +6,8 @@ from django.urls import include
 app_name = 'signinAdmin'
 
 urlpatterns = [
-    # Rutas existentes
     path('', views.vistaSignUpAdmin, name='admin_signup'),
+    path('verify/', views.verify, name='verify'),
+    path('confirmar/<str:token>/', views.confirm_email, name='confirm_email'),
+    path('verificacion-pendiente/', views.vistaVerificacionPendiente, name='verificacion_pendiente'),
 ]
