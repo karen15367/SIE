1febc6c7b3fe802fa55f37831819dab19a03c2f0
Samuel G <l22020779@veracruz.tecnm.mx>
2025-05-22 12:22:11 -0600

    fif

--- encuesta/views.py
+++ encuesta/views.py
@@ -109,13 +109,8 @@ def e2(request):
         datos.update({
             'carrera': request.POST.get('carrera'),
             'especialidad': request.POST.get('Especialidad'),
-            'fechaIngreso': datetime.datetime.strptime(
-                fi, '%Y-%m-%d').date(),
-            'fechaEgreso': datetime.datetime.strptime(
-                fe, '%Y-%m-%d').date(),
-            'fechaIngreso':datetime.datetime.strptime(
-                request.POST.get('fechaingreso'), '%Y-%m-%d').date() ,
-            'fechaEgreso':request.POST.get('fechaEgreso') ,
+            'fechaIngreso': request.POST.get('fechaingreso'),
+            'fechaEgreso':  request.POST.get('fechaEgreso'),
             'titulado': request.POST.get('titulo'),
             'dominioIngles': request.POST.get('dominio'),
             'otroIdioma': request.POST.get('idioma'),
